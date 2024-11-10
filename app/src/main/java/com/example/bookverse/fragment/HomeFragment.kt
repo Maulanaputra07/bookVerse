8b746f01ec2cca8472b198f5d3ed41b4950c48c9
Maulanaputra07 <maulanaaputraaa077@gmail.com>
2024-11-10 07:48:48 +0700

    Display buku

--- app/src/main/java/com/example/bookverse/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/bookverse/fragment/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.bookverse.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,12 +10,21 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import com.bumptech.glide.Glide
 import com.example.bookverse.user.DetailBookActivity
 import com.example.bookverse.R
 import com.example.bookverse.user.SeeAllActivity
 import com.example.bookverse.databinding.FragmentHomeBinding
+import com.example.bookverse.model.Books
+import com.google.firebase.FirebaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import java.util.Calendar
 
 // TODO: Rename parameter arguments, choose names that match
@@ -29,6 +39,8 @@ private const val ARG_PARAM2 = "param2"
  */
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var databaseReference: DatabaseReference
+    private val bookList = mutableListOf<Books>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +48,11 @@ class HomeFragment : Fragment() {
     ): View? {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        databaseReference = FirebaseDatabase.getInstance().getReference("books")
+
+        loadbooks()
+//        Log.d("listBukuu", bookList.toString())
+
         val view = binding.root
 
         val sharedPreferences = activity?.getSharedPreferences("userSession", AppCompatActivity.MODE_PRIVATE)
@@ -80,34 +97,68 @@ class HomeFragment : Fragment() {
 
 
         // Looping untuk menambahkan card
-        for (i in titles.indices) {
-            val cardViewRec = CreateCardView(titles[i], images[i], idBooks[i])
-            val cardViewNew = CreateCardView(titles[i], images[i], idBooks[i])
-            binding.linearBookRec.addView(cardViewRec)
-            binding.linearBookNew.addView(cardViewNew)
-
-            cardViewRec.setOnClickListener {
-                val intent = Intent(requireContext(), DetailBookActivity::class.java).apply {
-                    putExtra("bookId", idBooks[i])
-                    putExtra("title", titles[i])
-                }
-                startActivity(intent)
-            }
-
-            cardViewNew.setOnClickListener {
-                val intent = Intent(requireContext(), DetailBookActivity::class.java).apply {
-                    putExtra("bookId", idBooks[i])
-                }
-                startActivity(intent)
-            }
-
-
-        }
+//        for (i in titles.indices) {
+//            val cardViewRec = CreateCardView(titles[i], images[i], idBooks[i])
+//            val cardViewNew = CreateCardView(titles[i], images[i], idBooks[i])
+//            binding.linearBookRec.addView(cardViewRec)
+//            binding.linearBookNew.addView(cardViewNew)
+//
+//            cardViewRec.setOnClickListener {
+//                val intent = Intent(requireContext(), DetailBookActivity::class.java).apply {
+//                    putExtra("bookId", idBooks[i])
+//                    putExtra("title", titles[i])
+//                }
+//                startActivity(intent)
+//            }
+//
+//            cardViewNew.setOnClickListener {
+//                val intent = Intent(requireContext(), DetailBookActivity::class.java).apply {
+//                    putExtra("bookId", idBooks[i])
+//                }
+//                startActivity(intent)
+//            }
+//
+//
+//        }
 
         return view
     }
 
-    private fun CreateCardView(title: String, image: Int, id: Int): CardView{
+    private fun loadbooks(){
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                bookList.clear()
+                for(bookSnapShot in snapshot.children){
+                    val book = bookSnapShot.getValue(Books::class.java)
+                    if(book != null){
+                        bookList.add(book)
+                    }
+                }
+                displayBook()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("FirebaseError", "Database error: ${error.message}")
+                Toast.makeText(requireContext(), "Failed to load data: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun displayBook(){
+        binding.linearBookNew.removeAllViews()
+        binding.linearBookRec.removeAllViews()
+
+        for(book in bookList){
+            Log.d("bukuu", book.judul.toString())
+            val cardViewRec = CreateCardView(book)
+            val cardViewNew = CreateCardView(book)
+
+            binding.linearBookRec.addView(cardViewRec)
+            binding.linearBookNew.addView(cardViewNew)
+        }
+    }
+
+    private fun CreateCardView(book : Books): CardView{
         val cardView = CardView(requireContext()).apply {
             layoutParams = LinearLayout.LayoutParams(380, 500).apply {
                 setMargins(15, 15, 15, 15)
@@ -133,12 +184,15 @@ class HomeFragment : Fragment() {
             ).apply {
                 bottomMargin = 8
             }
-            setImageResource(image)
-            contentDescription = title
+            Glide.with(this)
+                .load(book.cover)
+                .into(this)
+            contentDescription = book.judul
         }
 
         val textView = TextView(requireContext()).apply {
-            text = title
+            Log.d("judulBuku", book.judul.toString())
+            text = book.judul
             textSize = 20f
             setTextColor(resources.getColor(android.R.color.black, null))
             layoutParams = LinearLayout.LayoutParams(
@@ -150,6 +204,19 @@ class HomeFragment : Fragment() {
         innerLayout.addView(imageView)
         innerLayout.addView(textView)
         cardView.addView(innerLayout)
+
+        cardView.setOnClickListener {
+            val intent = Intent(requireContext(), DetailBookActivity::class.java).apply {
+                putExtra("bookId", book.id)
+                putExtra("title", book.judul)
+                putExtra("penulis", book.penulis)
+                putExtra("tahunTerbit", book.tahun_terbit.toString())
+                putExtra("cover", book.cover)
+                putExtra("sinopsis", book.sinopsis)
+                putExtra("genre", book.genre)
+            }
+            startActivity(intent)
+        }
 
         return cardView
     }
--- app/src/main/java/com/example/bookverse/user/DetailBookActivity.kt
+++ app/src/main/java/com/example/bookverse/user/DetailBookActivity.kt
@@ -1,7 +1,9 @@
 package com.example.bookverse.user
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.bookverse.databinding.ActivityDetailBookBinding
 
 class DetailBookActivity : AppCompatActivity() {
@@ -16,10 +18,25 @@ class DetailBookActivity : AppCompatActivity() {
 
         val bookId = intent.getIntExtra("bookId", -1) // Mengambil ID buku
         val title = intent.getStringExtra("title") // Mengambil judul buku
+        val sinopsis = intent.getStringExtra("sinopsis")
+        val cover = intent.getStringExtra("cover")
+        val genre = intent.getStringExtra("genre")
+        val tahunTerbit = intent.getStringExtra("tahunTerbit").toString()
+        Log.d("tahun", tahunTerbit)
+        val penulis = intent.getStringExtra("penulis")
 
         // Mengatur nilai ke TextView
 //        binding.idBook.text = bookId.toString()
         binding.titleBook.text = title // Langsung mengatur judul
+        binding.sinopsis.text = sinopsis
+        cover.let {
+            Glide.with(this)
+                .load(it)
+                .into(binding.img)
+        }
+        binding.tvGenre.text = genre
+        binding.tvTahunTerbit.text = tahunTerbit
+        binding.tvPenulis.text = penulis
 
     }
 }
